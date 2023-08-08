46d1d4a2215b0bfda1745382b1a76560bc95dca0
Julieta Paz <juli_ines@hotmail.com>
2023-08-08 18:07:26 -0300

    probando

---
+++ menu.js
@@ -0,0 +1,6 @@
+const btnDropdown = document.getElementById('dropdown')
+const nav = document.getElementById('nav')
+btnDropdown.addEventListener('click', ()=>{
+    nav.classList.toggle('ocultar')
+    console.log("holamundo")
+})
